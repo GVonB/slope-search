b20d04f601d89b92eead049282f83572277f7430
Gunnar Von Bergen <vonbergb@miamioh.edu>
2025-05-06 22:56:43 -0400

    Added commands used to load data into the database after table creation

---
+++ sql/loadData.sql
@@ -0,0 +1,53 @@
+-- Load SkiArea table
+LOAD DATA LOCAL INFILE '/Users/gunnarvonbergen/development/slope-search/notebook/data/clean_ski_area.csv'
+INTO TABLE SkiArea
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(SkiAreaID, Country, Region, DownhillDistanceKm, VerticalM, MinElevationM, MaxElevationM, LiftCount, RunConvention, OpenSkiMap, Geometry, Latitude, Longitude);
+
+-- Load SkiAreaWebsite table
+LOAD DATA LOCAL INFILE '/Users/gunnarvonbergen/development/slope-search/notebook/data/clean_ski_area_website.csv'
+INTO TABLE SkiAreaWebsite
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(SkiAreaID, WebsiteURL);
+
+-- Load SkiAreaName table
+LOAD DATA LOCAL INFILE '/Users/gunnarvonbergen/development/slope-search/notebook/data/clean_ski_area_name.csv'
+INTO TABLE SkiAreaName
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(SkiAreaID, Name);
+
+-- Load Run table
+LOAD DATA LOCAL INFILE '/Users/gunnarvonbergen/development/slope-search/notebook/data/clean_run.csv'
+INTO TABLE Run
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(RunID, Country, Region, Difficulty, Color, Lit, InclinedLengthM, DescentM, AveragePitch, MaxPitch, MinElevationM, MaxElevationM, DifficultyConvention, OpenSkiMap, Geometry, Latitude, Longitude);
+
+-- Load RunName table
+LOAD DATA LOCAL INFILE '/Users/gunnarvonbergen/development/slope-search/notebook/data/clean_run_name.csv'
+INTO TABLE RunName
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(RunID, Name);
+
+-- Load SkiAreaRun table
+LOAD DATA LOCAL INFILE '/Users/gunnarvonbergen/development/slope-search/notebook/data/clean_ski_area_run.csv'
+INTO TABLE SkiAreaRun
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(RunID, SkiAreaID);
