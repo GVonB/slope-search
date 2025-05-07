ada6f0c35085ab7698a742baa99cb6bc49230741
Gunnar Von Bergen <vonbergb@miamioh.edu>
2025-05-07 17:21:39 -0400

    Add connection pooling

--- server/src/app.js
+++ server/src/app.js
@@ -5,11 +5,15 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-const db = mysql.createConnection({
+// Using connection pooling
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 db.connect(err => {
