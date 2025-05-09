c5df1e2ab0405eb962c55b79ddb11937bf2b9b4a
Gunnar Von Bergen <vonbergb@miamioh.edu>
2025-05-09 02:17:22 -0400

    Add camelCase aliases and dynamic querying

--- server/src/controllers/runController.js
+++ server/src/controllers/runController.js
@@ -3,6 +3,7 @@ const handleQuery = require('../utils/handleQuery');
 
 // TODO: Redis caching for this query?
 exports.getRuns = (req, res) => {
+    const { country, region, difficulty, color, minInclinedLength, minAveragePitch, orderBy, sortOrder } = req.query;
 
     // For a useful query we need the run name, and the
     // name of the ski area the run is at. However,
@@ -10,8 +11,25 @@ exports.getRuns = (req, res) => {
     // run name, and another for the ski area name.
     // Luckily, using MIN() should prioritize English
     // names, just as it appears to in the ski area queries.
-    const query = `
-        SELECT r.*,
+    let baseQuery = `
+        SELECT
+            r.RunID as runId,
+            r.Country as country,
+            r.Region as region,
+            r.Difficulty as difficulty,
+            r.Color as color,
+            r.Lit as lit,
+            r.InclinedLengthM as inclinedLengthM,
+            r.DescentM as descentM,
+            r.AveragePitch as averagePitch,
+            r.MaxPitch as maxPitch,
+            r.MinElevationM as minElevationM,
+            r.MaxElevationM as maxElevationM,
+            r.DifficultyConvention as difficultyConvention,
+            r.OpenSkiMap as openSkiMap,
+            r.Geometry as geometry,
+            r.Latitude as latitude,
+            r.Longitude as longitude,
             MIN(rn.Name) AS primaryRunName,
             MIN(san.Name) AS skiAreaName
         FROM Run r
@@ -19,10 +37,48 @@ exports.getRuns = (req, res) => {
         LEFT JOIN SkiAreaRun sar ON r.RunID = sar.RunID
         LEFT JOIN SkiArea sa ON sar.SkiAreaID = sa.SkiAreaID
         LEFT JOIN SkiAreaName san ON sa.SkiAreaID = san.SkiAreaID
-        GROUP BY r.RunID
+        WHERE 1=1
     `;
 
-    pool.query(query, (err, results) => {
+    const params = [];
+    
+    // Filter like Ski Areas, adding to query if needed
+    if (country) {
+        baseQuery += ' AND r.Country = ?';
+        params.push(country);
+    }
+    if (region) {
+        baseQuery += ' AND r.Region = ?';
+        params.push(region);
+    }
+    if (difficulty) {
+        baseQuery += ' AND r.Difficulty = ?';
+        params.push(difficulty);
+    }
+    if (color) {
+        baseQuery += ' AND r.Color = ?';
+        params.push(color);
+    }
+    if (minInclinedLength) {
+        baseQuery += ' AND r.InclinedLengthM >= ?';
+        params.push(Number(minInclinedLength));
+    }
+    if (minAveragePitch) {
+        baseQuery += ' AND r.AveragePitch >= ?';
+        params.push(Number(minAveragePitch));
+    }
+
+    baseQuery += ' GROUP BY r.RunID';
+    
+    if (orderBy) {
+        const allowedOrder = ['InclinedLengthM', 'AveragePitch', 'MaxPitch', 'DescentM'];
+        if (allowedOrder.includes(orderBy)) {
+            const direction = (sortOrder && sortOrder.toUpperCase() === 'ASC') ? 'ASC' : 'DESC';
+            baseQuery += ` ORDER BY r.${orderBy} ${direction}`;
+        }
+    }
+
+    pool.query(baseQuery, params, (err, results) => {
         handleQuery(err, results, res);
     });
 };
