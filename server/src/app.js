7574070a533ad2c936b55ebd11966c63f33120df
Gunnar Von Bergen <vonbergb@miamioh.edu>
2025-05-09 01:49:27 -0400

    Add user controller and routes

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const skiAreaRoutes = require('./routes/skiAreaRoutes');
 const runRoutes = require('./routes/runRoutes');
 const favoritesRoutes = require('./routes/favoritesRoutes');
+const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -12,6 +13,8 @@ app.use('/api/ski-areas', skiAreaRoutes);
 app.use('/api/runs', runRoutes);
 
 app.use('/api/favorites', favoritesRoutes);
+
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Ski API is running!');
---
+++ server/src/controllers/userController.js
@@ -0,0 +1,31 @@
+const pool = require('../config/db');
+const handleQuery = require('../utils/handleQuery');
+
+// Add a new user
+exports.addUser = (req, res) => {
+    const { username } = req.body;
+    const query = 'INSERT INTO User (Username) VALUES (?)';
+    pool.query(query, [username], (err, results) => {
+        // More accurate query response than generic handleQuery()
+        if (err) return res.status(500).json({ error: 'Failed to add user'});
+        res.json({ message: 'User added successfully', userId: results.insertId });
+    });
+};
+
+// Get all users (dev use)
+exports.getAllUsers = (req, res) => {
+    const query = 'SELECT * FROM User';
+    pool.query(query, (err, results) => {
+        handleQuery(err, results, res);
+    })
+}
+
+// Delete user by ID
+exports.deleteUserById = (req, res) => {
+    const { userId } = req.params;
+    const query = 'DELETE FROM User WHERE UserID = ?';
+    pool.query(query, [userId], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Failed to delete user' });
+        res.json({ message: 'User deleted successfully' });
+    });
+};
