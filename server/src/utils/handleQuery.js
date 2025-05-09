7d6b5e697d60890f7fa1513227d38d929c86cc34
Gunnar Von Bergen <vonbergb@miamioh.edu>
2025-05-09 01:35:51 -0400

    Only check for length of results if results is an array

--- server/src/utils/handleQuery.js
+++ server/src/utils/handleQuery.js
@@ -2,7 +2,7 @@ module.exports = function handleQuery(err, results, res) {
     if (err) {
         return res.status(500).json({ error: 'Database query failed' });
     }
-    if (results.length === 0) {
+    if (Array.isArray(results) && results.length === 0) {
         return res.status(404).json({ error: 'No results found' });
     }
     res.json(results);
