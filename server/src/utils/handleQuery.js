71340a9e6f4bccda074a28f6079be4855a42c9a2
Gunnar Von Bergen <vonbergb@miamioh.edu>
2025-05-09 02:44:48 -0400

    Flesh out handleQuery() with a status object

--- server/src/utils/handleQuery.js
+++ server/src/utils/handleQuery.js
@@ -1,9 +1,29 @@
-module.exports = function handleQuery(err, results, res) {
+module.exports = function handleQuery(err, results, res, options = {}) {
+    // Generic errors, able to be swapped to custom through parameters
+    const {
+        errorMessage = 'Database query failed',
+        notFoundMessage = 'No results found',
+        successMessage = null
+    } = options;
+
     if (err) {
-        return res.status(500).json({ error: 'Database query failed' });
+        return res.status(500).json({ error: errorMessage });
     }
+
     if (Array.isArray(results) && results.length === 0) {
-        return res.status(404).json({ error: 'No results found' });
+        return res.status(404).json({ error: notFoundMessage });
     }
+
+    if (results.affectedRows !== undefined) {
+        if (results.affectedRows === 0) {
+            return res.status(404).json({ error: notFoundMessage });
+        }
+        return res.json({ message: successMessage });
+    }
+
+    if (successMessage) {
+        return res.json({ message: successMessage, data: results});
+    }
+
     res.json(results);
 }
