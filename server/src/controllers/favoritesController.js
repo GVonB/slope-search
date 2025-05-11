4d2750b2e267b07411ec84762ffac1cf4aa1b404
Gunnar Von Bergen <vonbergb@miamioh.edu>
2025-05-10 20:26:23 -0400

    Make favorites call function like ski area call

--- server/src/controllers/favoritesController.js
+++ server/src/controllers/favoritesController.js
@@ -4,10 +4,37 @@ const handleQuery = require('../utils/handleQuery');
 // Get all favorites for a user based on their UserID
 exports.getFavoritesByUserId = (req, res) => {
     const { userId } = req.params;
+    // Admittedly, this is near complete code duplication from getSkiAreas.
+    // Right now, this is just saving a refactor of the ski area controller
+    // handling favorites in itself with user ids.
     const query = `
-        SELECT f.*, sa.*, MIN(san.Name) AS primaryName
+        SELECT 
+            sa.SkiAreaID as skiAreaId,
+            sa.Country as country,
+            sa.Region as region,
+            sa.DownhillDistanceKm as downhillDistanceKm,
+            sa.VerticalM as verticalM,
+            sa.MinElevationM as minElevationM,
+            sa.MaxElevationM as maxElevationM,
+            sa.LiftCount as liftCount,
+            sa.RunConvention as runConvention,
+            sa.OpenSkiMap as openSkiMap,
+            sa.Geometry as geometry,
+            sa.Latitude as latitude,
+            sa.Longitude as longitude,
+            MIN(san.Name) AS primaryName,
+            COUNT(DISTINCT r.RunID) AS runCount, 
+            MAX(r.AveragePitch) AS maxAverageRunPitch,
+            SUM(CASE WHEN r.Color = 'black' THEN 1 ELSE 0 END) AS blackCount,
+            SUM(CASE WHEN r.Color = 'blue' THEN 1 ELSE 0 END) AS blueCount,
+            SUM(CASE WHEN r.Color = 'green' THEN 1 ELSE 0 END) AS greenCount,
+            SUM(CASE WHEN r.Color = 'grey' THEN 1 ELSE 0 END) AS greyCount,
+            SUM(CASE WHEN r.Color = 'red' THEN 1 ELSE 0 END) AS redCount,
+            SUM(CASE WHEN r.Color = 'orange' THEN 1 ELSE 0 END) AS orangeCount
         FROM Favorites f
         JOIN SkiArea sa ON f.SkiAreaID = sa.SkiAreaID
+        LEFT JOIN SkiAreaRun sar ON sa.SkiAreaID = sar.SkiAreaID
+        LEFT JOIN Run r ON sar.RunID = r.RunID
         LEFT JOIN SkiAreaName san ON sa.SkiAreaID = san.SkiAreaID
         WHERE f.UserID = ?
         GROUP BY sa.SkiAreaID
